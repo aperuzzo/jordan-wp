21261c1cad6de4869adb41f1ee9e5e5666a0b0f9
Tony Peruzzo <anthony_peruzzo@hotmail.com>
2016-03-02 08:56:18 -0600

    add smooth scroll from menu

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 jQuery( window ).load(function() {
 	jQuery('.entry-content-home').each( function(){
 
-		// wrap all <p>'s except first in div
+		// wrap all <p>'s except first in div (make a read more button)
 		jQuery( this).children().slice(1).wrapAll('<div class="hide-p"></div>');
 
 	});
@@ -13,12 +13,25 @@ jQuery( window ).load(function() {
 	jQuery('.hide-p').after('<i class="fa fa-plus-square-o p-toggle"></i>');
 	jQuery('.hide-p').hide();
 	
-	
-	
 
 	stickyHeader();
 	pToggle();
 	
+	/* Scroll to specific section on front page via wordpress menu*/
+    jQuery(function() {
+        jQuery('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = jQuery(this.hash);
+                target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    jQuery('html,body').animate({
+                        scrollTop: (target.offset().top - 49)
+                    }, 500);
+                    return false;
+                }
+            }
+        });
+    });
 });
 
 
